a761cbd82be8f4da284817d823d648acda532b66
devendervb12 <info@s-max.in>
2018-02-22 17:19:27 +0530

    create update filter with component

--- WebContent/controller/ProductList.controller.js
+++ WebContent/controller/ProductList.controller.js
@@ -7,12 +7,15 @@ sap.ui.controller("smax.etag.PRODUCT.controller.ProductList", {
 */
 	onInit: function() {
 		
-		var url = "proxy/http/122.180.87.238:8000/sap/opu/odata/SAP/ZETAG_SRV/"
-		var oModel = new sap.ui.model.odata.v2.ODataModel(url);
+		var url = "proxy/http/122.180.87.238:8000/sap/opu/odata/SAP/ZETAG_SRV"
+		var oModel = new sap.ui.model.odata.v2.ODataModel(url); 
 		this.getView().setModel(oModel);
+		
+		//this.getView().getModel()
+		
 	},
 	onSearch : function(oEvent){
-		// get the value inserted in searchbox
+		// get the value inserted in search box
 		var query = oEvent.getSource().getValue(); 
 		debugger;
 		// filter List
@@ -41,6 +44,133 @@ sap.ui.controller("smax.etag.PRODUCT.controller.ProductList", {
 		this.getView().byId("idList").getBinding("items").filter(filters);
 		
 		}
+	},
+	onItemPress : function(oEvent){
+		// first get ProductID
+		 var prodID =  oEvent.getSource().getTitle(); 
+	//	debugger;
+		
+		//go and get the data
+		this.getView().byId("idSf").setVisible(true);
+		
+		// model url + 
+		this.getView().byId("idSf").bindElement("/ProductSet('" +prodID+  "')");
+		// get productDetails and mapp it to Simpleform
+		//show the simpleform with data
+		
+	},
+	onCreate : function(){
+	   
+		var oController = this;
+		
+		var odialog = new sap.m.Dialog({
+			title : "Enter Product Details",
+			content : [
+				new sap.m.Label({ text : "Product ID"}),
+				new sap.m.Input(),
+				new sap.m.Label({ text : "Name"}),
+				new sap.m.Input(),
+				new sap.m.Label({ text : "Category"}),
+				new sap.m.Input()				
+			],
+			buttons : [
+				new sap.m.Button({ text : "Save", press : function(){
+					// implement code for save in SAP
+					var data = {
+							ProductID : this.getParent().getContent()[1].getValue(),
+							Name : this.getParent().getContent()[3].getValue(),
+							Category : this.getParent().getContent()[5].getValue()
+					}
+					
+			
+					var oModel = this.getParent().getModel();
+					
+					oModel.create("/ProductSet", data, {
+						success : function(){ //callback function
+							//debugger;
+							sap.m.MessageToast.show("Data Created");
+								
+						},
+						error : function(){
+							sap.m.MessageToast.show("Data not Created");
+						//	debugger;
+						}
+					});
+					
+					
+				}}),
+				new sap.m.Button({text : "Cancel", press : function(){
+					odialog.close();
+				}})
+			]
+		});
+	var oModel = 	this.getView().getModel();
+	odialog.setModel(oModel);
+		odialog.open();
+		
+	},
+	onUpdate : function(oEvent){
+		
+		var oController = this;
+		
+		var odialog = new sap.m.Dialog({
+			title : "Update Product Name",
+			content : [
+				new sap.m.Label({ text : "Product ID"}),
+				new sap.m.Text({ text : oController.getView().byId("idSf").getContent()[1].getText()}),
+				new sap.m.Label({ text : "Name"}),
+				new sap.m.Input({ value : oController.getView().byId("idSf").getContent()[3].getText()})
+			],
+			buttons : [
+				new sap.m.Button({
+					text : "Update and Close",
+					press : function(){
+						
+						var prodId = oController.getView().byId("idSf").getContent()[1].getText();
+						// update logic
+						
+						var data = {
+								Name : this.getParent().getContent()[3].getValue()
+						}
+						
+						odialog.getModel().update("/ProductSet('"+oController.getView().byId("idSf").getContent()[1].getText()+"')", data, {
+							success : function(){
+								sap.m.MessageToast.show("Data updated");
+							},
+							error : function(){
+								sap.m.MessageToast.show("Data updated");
+							}
+						} )
+						
+						odialog.close();
+					}
+				})
+				
+				
+			]
+		});
+		
+		odialog.setModel(this.getView().getModel()).open();
+		
+	//  odialog.open();
+		
+		
 	}
-
+	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
